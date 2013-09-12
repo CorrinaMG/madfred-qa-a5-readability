15dd441600ec3c6340b859f771d5975aa1bd550e
Corrina Merasty-Gallant <student@192.168.0.28>
2013-09-12 13:37:39 -0400

    This is my most recent assignment #5 to you Clyde, from Corrina

--- src/DamageThisOne.java
+++ src/DamageThisOne.java
@@ -1,42 +1,30 @@
 import java.util.Scanner;
 
 public class DamageThisOne {
-
-	/**
-	 * @author Clyde Brown
-	 * @version 1.0
-	 * @date August 12, 2013
-	 * @param void
-	 */
 	
-	public static void main(String[] args) {
-		//This section declares variables and instantiates the Scanner sc
-		int ticketNumber, stem, checkDigit;
-		Scanner sc = new Scanner(System.in);
+	
+	
+		public static void main(String[] args) 
 		
-		/* This section requests input from the user, in the form of a 
-		 * six digit number, then it generates a stem and checkdigit 
-		 * from that number
+		//This section is a list of some things and that a box will apear sometime
+{int ticketNumber, stem, cD; Scanner sc = new Scanner(System.in);
+		
+		/* Some calculations 
 		 */
-		System.out.print("Please enter a six-digit ticket number: ");
-		ticketNumber = sc.nextInt();
-		stem = ticketNumber / 10;
-		checkDigit = ticketNumber % 10;	
+System.out.print("Please enter a six-digit ticket number: ");ticketNumber = sc.nextInt();stem = ticketNumber / 10;cD = ticketNumber % 10;	
 
-		/* This section will check the validity of the ticket number
-		 * and continue to prompt the user for a new ticket number
-		 * while the current number is invalid
+		/* checking the ticket
 		 */
-		while (checkDigit != stem % 7) {
-			System.out.println("That is NOT a valid ticket number, bucko!");
-			System.out.print("Try again: ");
-			ticketNumber = sc.nextInt();
-			stem = ticketNumber / 10;
-			checkDigit = ticketNumber % 10;	
-		}
-						
-		System.out.println("That is a valid ticket number, well done!");
-	
-	}
+while (cD != stem % 7) {
+System.out.println("That is NOT a valid ticket number, bucko!");
+System.out.print("Try again: ");ticketNumber = sc.nextInt();stem = ticketNumber / 10;cD = ticketNumber % 10;	
+}
+	//print message					
+System.out.println("That is a valid ticket number, well done!");
 
 }
+
+}
+
+
+//DONE BY CORRINA MERASTY-GALLANT
--- src/FixThisOne.java
+++ src/FixThisOne.java
@@ -1,33 +1,44 @@
 import java.util.Scanner;
+/**
+ * @author Corrina Merasty-Gallant
+ * @version 1.0
+ * @date September 12, 2013
+ * @param void
+ */
 
+//declaring the class of this code project
 public class FixThisOne {
 
-	public static void main(String[] args) 
-	{
-		//This program checks your airline tickets
-		 //This is a bynch of code dunno what it doez
-		final float PRICE_PER_DOZEN = 3.25f;final float PRICE_PER_SINGLE = 0.45f;
+	public static void main(String[] args) {
+		//This program will ask the user how many eggs they would like to order.
+		
+		//The next block of code is declaring 3 float variables and one int variable.
+		
+		final float PRICE_PER_DOZEN = 3.25f;
+		final float PRICE_PER_SINGLE = 0.45f;
 		int eggs, d, s;
-			float dozens_cost, singlesCost, tc;
-		Scanner kbrd = new Scanner(System.in);
+		float dozens_cost, singlesCost, totalCost;
+		// this is telling the program to accept the users input via the keyboard.
+		Scanner keyboard = new Scanner(System.in);
+		//user is prompted with the question of how many eggs are wanted.
 		System.out.print("How many eggs do you want? >> ");
-		eggs = kbrd.nextInt();
+		//customers data typed into the keyboard is a new variable named eggs, that holds the # input by user.
+		eggs = keyboard.nextInt();
+		//below are the methods declared to start calculating the amount of eggs input by the user.
 		d = eggs / 12;
 		s = eggs % 12;
 		dozens_cost = d * PRICE_PER_DOZEN;
 		singlesCost = s * PRICE_PER_SINGLE;
-		tc = dozens_cost + singlesCost;
+		totalCost = dozens_cost + singlesCost;
 		
-		/*This program checks your airline tickets
-		 * This is a block comment
-		 * Coding is fun!
+		/*
+		 *The below info will print out the results input by the user, after being passed through calculation in the methods above
+		 *and a the result will be printed on the screen.
 		 */
 		System.out.println("You ordered " + eggs + " eggs in total.");
-			System.out.println("That's " + d + " dozen at $3.25, or $" + dozens_cost);
-				System.out.println("That's " + s + " singles at 45c, or $" + singlesCost);
-				System.out.println("Your total cost is $" + tc);
-		
-	
+		System.out.println("That's " + d + " dozen at $3.25, or $" + dozens_cost);
+		System.out.println("That's " + s + " singles at 45 cents, or $ " + singlesCost);
+		System.out.println("Your total cost is $ " + totalCost);
 	}
 
 }
